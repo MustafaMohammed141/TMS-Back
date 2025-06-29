f399a119932ff63121c7659a14d76145a42eae7d
MustafaMohammed141 <mostafamohamed12434@gmail.com>
2025-06-29 19:59:25 +0300

    fix: improve CORS configuration and format response messages in app.js

--- app.js
+++ app.js
@@ -11,8 +11,21 @@ const { checkSign } = require("./MiddleWare/CheckSign");
 
 require("dotenv").config();
 
+const allowedOrigins = ["http://localhost:5173"];
+
+server.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 server.use(cookie());
-server.use(cors({ credentials: true ,origin: * }));
 server.use(express.json());
 
 mongoose
@@ -30,14 +43,18 @@ server.use("/auth", authRoutes);
 server.use("/isLogged", (req, res) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(200).json({ logged: false,message:"no token provided" });
+    return res
+      .status(200)
+      .json({ logged: false, message: "no token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
     return res.status(200).json({ logged: true });
   } catch (err) {
-    return res.status(403).json({ logged: false,message:"something went wrong" });
+    return res
+      .status(403)
+      .json({ logged: false, message: "something went wrong" });
   }
 });
 

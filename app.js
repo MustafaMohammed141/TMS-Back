b3be0955a245ddcf401002621bf5677df51151c1
Mustafa <68327651+MustafaMohammed141@users.noreply.github.com>
2025-07-05 18:47:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { checkSign } = require("./MiddleWare/CheckSign");
 
 require("dotenv").config();
 
-const allowedOrigins = ["http://localhost:5173","http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173"];
 
 server.use(
   cors({
